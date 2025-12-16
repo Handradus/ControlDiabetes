933dd50f02236421b050b0c46a69e5b0c7ebe94c
Lucas23-IECI <138260204+Lucas23-IECI@users.noreply.github.com>
2025-12-16 15:58:39 -0300

    ADD: Comentarios admin

--- src/modelo/Admin.java
+++ src/modelo/Admin.java
@@ -2,6 +2,11 @@ package modelo;
 
 public class Admin extends Usuario {
 
+    /*
+Usuario con rol administrador encargado de la gestión de cuentas de cuidadores.
+Separa las tareas administrativas del registro clínico del sistema
+ */
+    
     public Admin(String nombreUsuario, String password) {
         super(nombreUsuario, password);
     }
